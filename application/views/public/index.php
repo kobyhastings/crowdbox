c721665a3cf191385c2a7f79c58b4e43027694d2
kobyhastings <koby_hastings1@mymail.eku.edu>
2014-05-27 12:11:17 -0400

    new home page to push to the server

--- application/views/public/index.php
+++ application/views/public/index.php
@@ -4,13 +4,13 @@
 <div class="row" style="margin: 60px 0px;">
 	<div class="col-md-5 col-md-offset-7">
 		<div id="contentWrapper">
-			<?php echo form_open('event/index', 'id="enterEvent" class="enterCode"'); ?>
-				<?php echo form_input('eventcode', '', 'placeholder="Enter event code" class="input" autocomplete="off"'); ?>
-				<?php echo form_submit('submit', "Go"); ?>
-			<?php echo form_close(); ?>
-			<span style="color: #fff; text-align: center;">
-				<a href="<?php echo site_url(); ?>/signin/index" id="signInButton" style="text-align: center;">Sign in to create/manage an event</a> <!-- | <a href="#" id="signUpButton" style="text-align: center;">Or sign up here</a> -->
-			</span>
+			<?php //echo form_open('event/index', 'id="enterEvent" class="enterCode"'); ?>
+				<?php //echo form_input('eventcode', '', 'placeholder="Enter event code" class="input" autocomplete="off"'); ?>
+				<?php //echo form_submit('submit', "Go"); ?>
+			<?php //echo form_close(); ?>
+			<!-- <span style="color: #fff; text-align: center;">
+				<a href="<?php //echo site_url(); ?>/signin/index" id="signInButton" style="text-align: center;">Sign in to create/manage an event</a> <!-- | <a href="#" id="signUpButton" style="text-align: center;">Or sign up here</a> -->
+			<!-- </span> -->
 		</div>
 	</div>
 </div>
